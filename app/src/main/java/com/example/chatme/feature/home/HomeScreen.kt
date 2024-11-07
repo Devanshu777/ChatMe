83c4894ab4e51f57264ef85a2a5db7ed42b5384e
Devanshu Tiwari <92571427+Devanshu777@users.noreply.github.com>
2024-11-07 11:37:18 +0530

    channel implemented

--- app/src/main/java/com/example/chatme/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/chatme/feature/home/HomeScreen.kt
@@ -1,23 +1,36 @@
 package com.example.chatme.feature.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
     // Your home screen UI
+    val viewModel = hiltViewModel<HomeViewModel>()
+    val channels = viewModel.channel.collectAsState()
     Scaffold {
-        Column(modifier = Modifier
+        Box(modifier = Modifier
             .padding(it)
             .fillMaxSize()
-            .background(Color.Red)){
+        ){
+            LazyColumn {
+                items(channels.value){ channel ->
+                    Column {
+                        Text(text = channel.name)
+                    }
+                }
+            }
 
         }
     }
